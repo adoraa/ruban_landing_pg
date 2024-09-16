06e686d998c5367868c24e90afacbfd5cab24dfe
Adora <adoraonyile@gmail.com>
2024-09-16 12:58:17 +0100

    redirect

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins']
+      },
+      backgroundImage: {
+        'hero-img': "url('/images/bg-image.png')"
+      },
+      colors: {
+        primary: {
+          100: '#f0fdf4',
+          200: '#009933',
+          300: '#006633'
+        },
+      },
+      screens: {
+        'phone-s': '320px',
+        'phone-m': '375px',
+        'phone-l': '425px',
+        'p-mini': '540px'
+      },
+    },
+  },
+  plugins: [],
+}
