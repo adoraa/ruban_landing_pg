b0fcb88a9352318935e267bbf38b9588376840be
Adora <adoraonyile@gmail.com>
2024-09-23 12:02:57 +0100

    optimized for mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
         poppins: ['Poppins']
       },
       backgroundImage: {
-        'hero-img': "url('./src/images/bg-image01.png')"
+        'hero-img': "url('/images/bg-image01.png')"
       },
       colors: {
         primary: {
