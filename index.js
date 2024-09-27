07e97831898ed3d0ebc7710d89915c1fb4327314
Adora <adoraonyile@gmail.com>
2024-09-27 15:58:21 +0100

    added close svg to toggle navigation

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const nav_menu = document.querySelector('#nav_menu');
 navbar.addEventListener('click', () => {
     if (nav_menu.classList.contains('hidden')) {
         nav_menu.classList.remove('hidden');
+        navbar.src = "images/svg/close.svg";
     } else {
         nav_menu.classList.add('hidden');
+        navbar.src = "images/svg/hamburger.svg"
     }
 });
