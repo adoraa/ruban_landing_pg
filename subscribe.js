6da8ffe1ef420a9de0d87ea56ca2b34f4cb7be00
Adora <adoraonyile@gmail.com>
2024-10-17 16:38:42 +0100

    updated subscription form

--- subscribe.js
+++ subscribe.js
@@ -1,12 +1,17 @@
 window.onload = function() {
-    document.getElementById('subscribe').addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent form from submitting the default way
+    document.getElementById('subscribe').addEventListener('submit', (e) => {
+        e.preventDefault(); // Prevent form from submitting the default way
         // Show a loading message or disable the button
         const form = document.getElementById('subscribe');
         form.innerHTML = `<p class="text-blue-600 font-semibold">Submitting...</p>`;
 
+        //get user email from the form
+        const userEmail = {
+            email: email.value
+        };
+
         // Send form data via EmailJS
-        emailjs.sendForm('service_8i4eff3', 'template_lt2w09u', this)
+        emailjs.sendForm('service_8i4eff3', 'template_lt2w09u', email)
             .then(function(response) {
                 console.log('SUCCESS!', response.status, response.text);
                 //alert("Thank you for subscribing!");
