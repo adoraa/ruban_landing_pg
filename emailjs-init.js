86451e7b395f843cedf6e7b4c4b1cab28b07c177
adoraa <adoraa@users.noreply.github.com>
2024-12-10 09:51:41 +0000

    deploy: 769ff579017c5cde4ae14f440f74acc73793fbfd

--- emailjs-init.js
+++ emailjs-init.js
@@ -1,9 +1,11 @@
 // emailjs-init.js
-(function() {
-    emailjs.init("255k42oWLmBaaQqf8")
+(function () {
+  emailjs.init("255k42oWLmBaaQqf8");
 })();
 
-document.getElementById("subscribe").addEventListener("submit", function(event) {
+document
+  .getElementById("subscribe")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
 
     // Show a loading message & disable the button
@@ -15,22 +17,25 @@ document.getElementById("subscribe").addEventListener("submit", function(event) 
 
     // EmailJS template parameters
     const templateParams = {
-        email: email,
-        form_link: "https://forms.gle/H3pAPkK6w12DD9K37"
+      email: email,
+      form_link:
+        "https://docs.google.com/forms/d/e/1FAIpQLSck0xIjXRyO-0dWZXRgwKWQ_sk-3bTbBlv-WGkcOjbLLTMzRw/viewform?usp=sf_link",
     };
 
-    emailjs.send("service_8i4eff3", "template_ngna1ak", templateParams)
-        .then(function(response) {
-            console.log('SUCCESS!', response);
-            statusMessage.innerText = `
+    emailjs.send("service_8i4eff3", "template_ngna1ak", templateParams).then(
+      function (response) {
+        console.log("SUCCESS!", response);
+        statusMessage.innerText = `
                 Thank you for subscribing!
                 Please check your email for the next steps.
                 If you don’t see it, please check your spam or junk folder.
             `;
-            statusMessage.className = "text-green-600 text-sm font-semibold pt-4";
-        }, function(error) {
-            console.error("EmailJS Error:", error);
-            statusMessage.innerText = "Failed to send the email. Please try again.";
-            statusMessage.className = "text-red-600 text-sm font-semibold";
-        });
-});
+        statusMessage.className = "text-green-600 text-sm font-semibold pt-4";
+      },
+      function (error) {
+        console.error("EmailJS Error:", error);
+        statusMessage.innerText = "Failed to send the email. Please try again.";
+        statusMessage.className = "text-red-600 text-sm font-semibold";
+      }
+    );
+  });
